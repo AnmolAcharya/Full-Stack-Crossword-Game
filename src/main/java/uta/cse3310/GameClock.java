6001688dd3a1a4ad50b197264c216fbc2325e801
MoldyBread16 <loganlantrip11@gmail.com>
2024-04-12 17:33:56 -0500

    added game clock

--- src/main/java/uta/cse3310/GameClock.java
+++ src/main/java/uta/cse3310/GameClock.java
@@ -12,7 +12,7 @@ public class GameClock{
     	startTime = System.currentTimeMillis();
 	}
 
-	// Use this to get the time on a active game
+	// Use this to get the time on a active game and check if it is over 5 minutes
 	public String getTime() {
 		long elapsedTime = System.currentTimeMillis() - startTime;
 		long elapsedSeconds = elapsedTime / 1000;

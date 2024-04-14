9f55680235e2a64928edbe9a50fe6e11bf6a2b1d
MoldyBread16 <loganlantrip11@gmail.com>
2024-04-13 22:27:08 -0500

    Player Class Update

--- src/main/java/uta/cse3310/Player.java
+++ src/main/java/uta/cse3310/Player.java
@@ -5,23 +5,33 @@ public class Player{
 	public String userName;
 	public boolean ready;
 	public String color;
-	public int highscore;
+	public int highScore;
 	public int currentScore;
 
 	public Player(String uid) {
 		this.uid = uid;
+		this.currentScore = 0;
+		this.highScore = 0;
+		this.ready = false;
 	}
 
 	public void ready() {
-
+		ready = !ready;
 	}
 
-	public void updateScore() {
-
+	//call with number of points to add or with 0 to reset the current score
+	public void updateScore(int points) {
+		if(points != 0) {
+			currentScore = currentScore + points;
+		}
+		else {
+			currentScore = points;
+		}
 	}
 
 	public void updateHighscore() {
-		
+		if(currentScore > highScore) {
+			highScore = currentScore;
 	}
 
 }

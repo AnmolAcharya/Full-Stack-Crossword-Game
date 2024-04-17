a399c6d17ff63566f9aade0bc293412bbe79b6ce
MoldyBread16 <loganlantrip11@gmail.com>
2024-04-17 12:48:58 -0500

    started on Game unit test

---
+++ src/test/java/uta/cse3310/GameUnitTest.java
@@ -0,0 +1,30 @@
+package uta.cse3310;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import java.util.ArrayList;
+import java.util.UUID;
+import java.util.Collections;
+
+/**
+ * Unit test for Game class.
+ */
+public class GameUnitTest extends TestCase {
+    public GameUnitTest(String testName){
+        super(testName);
+    }
+
+    public static Test suite(){
+        return new TestSuite(GameUnitTest.class);
+    }
+/*
+    Player p1 = new Player("1");
+    public ArrayList<String> words = new ArrayList<String>();
+    words.add("test");
+    words.add("word");
+    public Lobby lobby = new Lobby();
+    port = (websocketPort != null ? Integer.parseInt(websocketPort) : 9102);
+    App A = new App(port);
+    Game test = new Game(p1, words, lobby, A); */
+}

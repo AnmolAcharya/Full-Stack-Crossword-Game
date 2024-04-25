24bd7d92fdeebf7a25187cca179de3d01ce769bc
aabishkar02 <axb8080@mavs.uta.edu>
2024-04-25 05:41:21 -0500

    updated

--- src/test/java/uta/cse3310/AppTest.java
+++ src/test/java/uta/cse3310/AppTest.java
@@ -4,29 +4,15 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import org.java_websocket.WebSocket;
-import org.java_websocket.drafts.Draft;
-import org.java_websocket.drafts.Draft_6455;
-import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
-
 import org.mockito.Mockito;
-import java.util.Timer;
-import java.util.HashMap;
-import java.util.Map;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-
-
 
 public class AppTest extends TestCase {
 
-    App app = new App(9002);
-    Gson gson = new Gson();
-    WebSocket webSocketMock = Mockito.mock(WebSocket.class);
+    private App app;
+    private Gson gson;
+    private WebSocket webSocketMock;
 
     public AppTest(String testName) {
         super(testName);
@@ -34,13 +20,24 @@ public class AppTest extends TestCase {
 
     public static Test suite() {
         return new TestSuite(AppTest.class);
-
     }
 
-    public void testHandleMessage() {
-    	app.getWords();
+    protected void setUp() throws Exception {
+        super.setUp();
+        app = new App(9002); // Assuming App class takes port number as constructor argument
+        gson = new Gson();
+        webSocketMock = Mockito.mock(WebSocket.class);
+
+        app.getWords();
         app.start();
-    	Player firstPlayer = new Player("xxaa");
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testValidateUsername() {
+        Player firstPlayer = new Player("xxaa");
         Player secondPlayer = new Player("xxbb");
 
         app.activeSessions.put("xxaa", firstPlayer);
@@ -48,35 +45,45 @@ public class AppTest extends TestCase {
 
         String firstPlayerUN = "aa";
         String firstPlayerID = "xxaa";
-        assertTrue("true", app.validateUsername(firstPlayerUN, firstPlayerID));
+        assertTrue("Username validation failed", app.validateUsername(firstPlayerUN, firstPlayerID));
 
         String secondPlayerUN = "aa";
         String secondPlayerID = "xxbb";
-        assertFalse("false", app.validateUsername(secondPlayerUN, secondPlayerID));
-
-               // Add your assertions or test logic here
-        String jsonMessage = "{\"screen\":\"lobby\",\"type\":\"createGame\",\"uid\":\"xxaa\"}";
-        
-        assertTrue("true", app.activeGames.size() == 0);
-        app.handleMessage(gson, jsonMessage, webSocketMock);
-        
-        assertTrue("true", app.activeGames.size() == 1);
-
- 
-        String gameId = app.activeGames.keySet().stream().findFirst().orElse(null);
-        String jsonMessage2 = "{\"screen\":\"lobby\",\"type\":\"joinGame\",\"uid\":\"xxbb\", \"gameId\":\""+gameId +"\"}";
-        assertTrue("true", app.activeGames.get(gameId).players.size() == 1);
-        app.handleMessage(gson, jsonMessage2, webSocketMock);
-        assertTrue("true", app.activeGames.get(gameId).players.size() == 2);
-        
-        String jsonMessage3 = "{\"screen\":\"lobby\",\"type\":\"leaveGame\",\"uid\":\"xxbb\", \"gameId\":\""+gameId +"\"}";
-        assertTrue("players before leaveGame", app.activeGames.get(gameId).players.size() == 2);
-        app.handleMessage(gson, jsonMessage3, webSocketMock);
-        assertTrue("players after leaveGame", app.activeGames.get(gameId).players.size() == 1);
-
-
+        assertFalse("Username validation failed", app.validateUsername(secondPlayerUN, secondPlayerID));
     }
 
+    public void testhandleMessage() {
+        Player firstPlayer = new Player("xxaa");
+        Player secondPlayer = new Player("xxbb");
+
+        app.activeSessions.put("xxaa", firstPlayer);
+        app.activeSessions.put("xxbb", secondPlayer);
+
+        String firstPlayerUN = "aa";
+        String firstPlayerID = "xxaa";
+
+        String secondPlayerUN = "aa";
+        String secondPlayerID = "xxbb";
+        
+        String jsonMessage = "{\"screen\":\"lobby\",\"type\":\"createGame\",\"uid\":\"xxaa\"}";
+
+        assertTrue("Active games size before creation is not 0", app.activeGames.size() == 0);
+        app.handleMessage(gson, jsonMessage, webSocketMock);
+        assertTrue("Active games size after creation is not 1", app.activeGames.size() == 1);
+    
+        String gameId = app.activeGames.keySet().stream().findFirst().orElse(null);
+        String jsonMessage2 = "{\"screen\":\"lobby\",\"type\":\"joinGame\",\"uid\":\"xxbb\", \"gameId\":\"" + gameId + "\"}";
+        assertTrue("Players size before joining is not 1", app.activeGames.get(gameId).players.size() == 1);
+        app.handleMessage(gson, jsonMessage2, webSocketMock);
+        assertTrue("Players size after joining is not 2", app.activeGames.get(gameId).players.size() == 2);
     
         
+        String jsonMessage3 = "{\"screen\":\"lobby\",\"type\":\"leaveGame\",\"uid\":\"xxbb\", \"gameId\":\"" + gameId + "\"}";
+        assertTrue("Players size before leaving is not 2", app.activeGames.get(gameId).players.size() == 2);
+        app.handleMessage(gson, jsonMessage3, webSocketMock);
+        assertTrue("Players size after leaving is not 1", app.activeGames.get(gameId).players.size() == 1);
+    
+    }
+
+
 }

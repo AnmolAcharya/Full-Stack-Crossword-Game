a325003b911020f9da24574c9ddb10f6d840a3b7
Anmol-Acharya <anmolacharya94@gmail.com>
2025-06-09 15:50:11 -0500

    backendish

--- src/main/java/uta/cse3310/GameObserver.java
+++
@@ -1,7 +0,0 @@
-package uta.cse3310;
-
-// Will allow every game object to directly access the WebSockets broadcast method, for when the game ends
-public interface GameObserver {
-    void notifyGameEnd(Game game);
-    void sendGameHint(Letter hint, String gameId);
-}
